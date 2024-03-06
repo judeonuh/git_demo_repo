b188d655280142dd544a0afd73b3d2f2b9743dd3
jude prince onuh <judeprinceonuh@gmail.com>
2024-03-06 16:52:18 +0000

    issue-1 fixed

---
+++ helloUser.py
@@ -0,0 +1,8 @@
+"""
+This program requests user name,
+and prints a greeting message.
+"""
+
+username = input("What is your name: ")
+user_mess = "You are welcome to my branch!"
+print(f"Hello {username} !\n{user_mess}")
