61dcc4bcbd0511974f17fce31a180cc7726c18b3
jude prince onuh <judeprinceonuh@gmail.com>
2024-03-06 17:49:23 +0000

    variable for user message changed

--- helloUser.py
+++ helloUser.py
@@ -1,4 +1,8 @@
 """
 This program requests user name,
-and prints a greeting message.
+and prints a greeting message to the user.
 """
+
+username = input("What is your name: ")
+user_greeting = "You are welcome to my branch!"
+print(f"Hello {username} !\n{user_greeting}")
