63af494f77dbc19aa146720f8b45e858fd59ade2
jude prince onuh <judeprinceonuh@gmail.com>
2024-03-06 16:21:23 +0000

    Print out user input

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,8 @@
 """
-This is a simple program that prints out the message:
-"Hello World!"
+This is a simple program that prints a message to the user
 """
 
 print("Git is awesome!")
+
+user_mess = input ("Enter any message of your choice: ")
+print(user_mess)
